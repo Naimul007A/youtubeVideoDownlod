858f0670ea0319a68c716e032827b4b2fe8a5229
Naimul <mdnaimul2090@gmail.com>
2023-08-22 02:51:04 +0600

    youtube video download script

---
+++ handler/download.js
@@ -0,0 +1,22 @@
+const puppeteer = require("puppeteer");
+const download = async (req, res) => {
+    const url =req.body.url;
+    const browser = await puppeteer.launch({
+        headless: true,
+        defaultViewport:{width: 1366, height: 768},
+    });
+    const page = await browser.newPage();
+    await page.goto('https://en.savefrom.net/1-youtube-video-downloader-533nN/',{waitUntil: 'networkidle2'});
+    await page.waitForSelector("#sf_url");
+    const search = await page.$("#sf_url");
+    await search.type(url);
+    await page.click(".r-box button");
+    await page.waitForSelector(".result-box");
+    const download = await page.$('.def-btn-box a[href]');
+    const downloadLink = await page.evaluate(download => download.href, download);
+    // await console.log(downloadLink);
+    // await console.log("downloadLink founded");
+    await browser.close();
+    res.render('download',{url:downloadLink});
+}
+module.exports = download;
---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const download = require('./handler/download')
+const app = express()
+const port = 3200
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+app.get('/',function(req,res){
+    res.render('index');
+});
+app.post('/download',download )
+app.get('/download',function(req,res){
+    console.log(req.query.url);
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+module.exports = app;
