367885a71ccdb34aba2748f2540cfa0fb93d0f06
Naimul <mdnaimul2090@gmail.com>
2023-08-22 03:26:09 +0600

    view change

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express')
 const download = require('./handler/download')
 const app = express()
 const port = 3200
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs'); // Replace 'ejs' with your view engine if different
+app.set('views', __dirname+'/views');
 app.use(express.urlencoded({ extended: true }));
 app.get('/',function(req,res){
     res.render('home');
