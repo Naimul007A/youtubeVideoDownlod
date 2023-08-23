4fd5bf9d486d0c0433eafec0aa29d44436d25989
Naimul <mdnaimul2090@gmail.com>
2023-08-23 20:50:53 +0600

    some change

--- handler/download.js
+++ handler/download.js
@@ -1,7 +1,6 @@
-
+const puppeteer = require("puppeteer");
+const chromium = require('chrome-aws-lambda');
 const download = async (req, res) => {
-    const puppeteer = require("puppeteer-core");
-    const chromium = require('chrome-aws-lambda');
     const url =req.body.url;
     const browser = await puppeteer.launch({
         ignoreDefaultArgs: ['--disable-extensions'],
