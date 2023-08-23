b6095e575ff9bd0aaf3d4783bc2b12aaaf2a468e
Naimul <mdnaimul2090@gmail.com>
2023-08-23 15:19:07 +0600

    some change

--- handler/download.js
+++ handler/download.js
@@ -16,11 +16,23 @@ const download = async (req, res) => {
     await search.type(url);
     await page.click(".r-box button");
     await page.waitForSelector(".result-box");
+    const image =await page.$('img.thumb');
+    const imageLink = await page.evaluate(image => image.src, image);
+    const titleHandel = await page.$('.info-box .title');
+    const title = await page.evaluate(titleHandel => titleHandel.textContent, titleHandel);
+    const durationHandel = await page.$('.info-box .duration');
+    const duration = await page.evaluate(durationHandel => durationHandel.textContent, durationHandel);
     const download = await page.$('.def-btn-box a[href]');
     const downloadLink = await page.evaluate(download => download.href, download);
     // await console.log(downloadLink);
     // await console.log("downloadLink founded");
     await browser.close();
-    res.render('download',{url:downloadLink});
+    const result = {
+        imageLink: imageLink,
+        url: downloadLink,
+        title: title,
+        duration: duration
+    }
+    res.render('download',{result:result});
 }
 module.exports = download;
--- index.js
+++ index.js
@@ -4,11 +4,16 @@ const app = express()
 const port = 3200
 app.set('view engine', 'ejs'); // Replace 'ejs' with your view engine if different
 app.set('views', __dirname+'/views');
+app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }));
 app.get('/',function(req,res){
     res.render('home');
 });
-app.get('/download',download )
+app.route('/download')
+    .get(function(req,res){
+        res.render('getdownload');
+    })
+    .post(download)
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
